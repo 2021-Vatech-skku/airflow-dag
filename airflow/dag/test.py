fd8d53192abc018ac899eab6d1ea35fbd76038d0
Heegwan Son <gmlrhks95@gmail.com>
2021-08-04 17:13:44 +0900

    [debug] set sample code

--- airflow/dag/test.py
+++ airflow/dag/test.py
@@ -1,46 +1,62 @@
-from datetime import timedelta
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+This is an example dag for using the KubernetesPodOperator.
+"""
+
+from kubernetes.client import models as k8s
+
 from airflow import DAG
-from airflow import models
 from airflow.kubernetes.secret import Secret
+from airflow.operators.bash import BashOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
 
-args = {
-    'owner' : 'gmlrhks95',
-    'start_date' : days_ago(2),
-    'retries': 2,
-    'retry_delay': timedelta(minutes=1),
-}
-
 with DAG(
-    dag_id='elastic_faker_example', 
-    schedule_interval='@daily',
-    default_args=args,
-    tags=['faker'],
-    ) as dag:
-
-    test_elastic_faker = KubernetesPodOperator(
-        namespace='elasticsearch',
-        image='gmlrhks95/elastic-faker',
-        name='elastic-faker-pods',
-        is_delete_operator_pod=False,
-        in_cluster=True,
-        task_id="elastic-faker",
-        get_logs=True,
-        dag=dag,
+    dag_id='example_kubernetes_operator',
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['example'],
+) as dag:
+    k = KubernetesPodOperator(
+        namespace='default',
+        image="ubuntu:16.04",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task",
+        is_delete_operator_pod=True,
+        hostnetwork=False,
+        priority_class_name="medium",
     )
 
-    test_echo = KubernetesPodOperator(
-        namespace='elasticsearch',
-        image='busybox',
-        name='echoing',
-        is_delete_operator_pod=False,
+    # [START howto_operator_k8s_write_xcom]
+    write_xcom = KubernetesPodOperator(
+        namespace='default',
+        image='alpine',
+        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
+        name="write-xcom",
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
         in_cluster=True,
-        task_id="echoing",
-        cmds=['echo'],
-        arguments=['Hello World!'],
+        task_id="write-xcom",
         get_logs=True,
-        dag=dag,
     )
 
-    test_echo >> test_elastic_faker
+    k >> write_xcom
