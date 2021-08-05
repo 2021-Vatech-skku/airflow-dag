a348ef87c00a0ead87f1ab4085aeb6bcaf322f15
Heegwan Son <gmlrhks95@gmail.com>
2021-08-05 17:11:30 +0900

    [debug] set namespace local

--- airflow/dag/test.py
+++ airflow/dag/test.py
@@ -77,7 +77,7 @@ with DAG(
     FakerOperator = KubernetesPodOperator(
         task_id='elasticfaker-test',
         name="elastic_faker",
-        namespace="elasticsearch",
+        namespace="airflow",
         image="gmlrhks95/elastic-faker",
         ports=[port],
         env_vars={'PYTHONUNBUFFERED' : 'True'},
