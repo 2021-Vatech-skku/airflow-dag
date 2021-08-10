624cef6b8cefefcb46fa200b8cc239fa229f3309
Baek, Song Hyun <cmcm0012@gmail.com>
2021-08-10 16:51:57 +0900

    module modified

--- airflow/dag/tt.py
+++ airflow/dag/tt.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from airflow.utils.dates import days_ago
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -9,7 +9,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
  
 
 def branch_func(ti):
-    xcom_value = ti.xcom_pull(key="daily", execution_date=timedelta.datetime(2021, 8, 5))
+    xcom_value = ti.xcom_pull(key="daily", execution_date=datetime(2021, 8, 5))
     if xcom_value:
         if xcom_value >= 5:
             return 'daily'
@@ -19,7 +19,7 @@ def branch_func(ti):
         return 'overwrite'
 
 def print_xcom(ti):
-    xcom_value = ti.xcom_pull(key="daily", execution_date=timedelta.datetime(2021, 8, 5))
+    xcom_value = ti.xcom_pull(key="daily", execution_date=datetime(2021, 8, 5))
     ti.xcom_push(key="check", value=xcom_value)
     return xcom_value
 
@@ -28,7 +28,7 @@ def xcom_daily(ti):
 
 args = {
       'owner' : 'Sanhak',
-      'start_date' : timedelta.datetime(2021, 8, 5),    #'start_date': datetime(2021, 8, 8),
+      'start_date' : datetime(2021, 8, 5),    #'start_date': datetime(2021, 8, 8),
       'retries' : 2,
       'retry_delay' : timedelta(minutes=3),
       'provide_context': True
